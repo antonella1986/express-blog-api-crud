4ce8bcc2c51b889b4821f8fa02d2a2e33db81d49
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-14 16:10:20 +0100

    add logic

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,14 +1,19 @@
-const postController = require("../controllers/postController");
-const postsData = require("../data/postsList");
+const posts = require("../data/postsData");
+const postsData = require("../data/postsData");
 
 //index
 function index(req, res){
-    res.send("Lista dei post");
+    res.json(postsData);
 }
 
 //show
 function show(req, res){
-    res.send(`Dettagli del post con id: ${req.params.id}`);
+    const postId = Number(req.params.id);
+    //cerco il l'id che mi interessa dentro postData (l'array di oggetti)
+    //(post rappresenta ogni elemento dell'array postsData
+    //se il post.slug è uguale all'id che mi interessa, allora viene restituito
+    const post = postsData.find(post => post.id === postId);
+    res.json(`Restituisci post con id: ${postId}`);
 }
 
 //store
@@ -28,7 +33,18 @@ function modify(req, res){
 
 //destroy
 function destroy(req, res){
+    const postId = Number(req.params.id);
+    // cerco il post con l'id passato
+    const post = postsData.find(post => post.id === postId);
+    // imposto il 404 se il post cercato non esiste
+    if (!post) {
+        return res.status(404).json({
+            error: 'Post non trovato',
+            message: `Post con id ${req.params.id} non trovato`
+        });
+    }
     res.send(`Eliminazione del post con id: ${req.params.id}`);
+    console.log(postsData)
 }
 
 module.exports = { index, show, store, update, modify, destroy };
